5cee07e6c0090149e4e9f02d4c1dfd8dfd178c22
ignas88 <ignas.c88@gmail.com>
2024-02-29 00:42:21 +0200

    wp plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,49 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        }),
+    ],
     output: {
-        filename: "app.js",
-        path: path.resolve(__dirname, "dist")
-    }
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "./",
+        assetModuleFilename: "images/[name][ext][query]"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [`...`, new CssMinimizerPlugin(),],
+    },
 }
